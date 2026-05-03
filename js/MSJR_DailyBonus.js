8c36245de13815e72994d3ebdfc2ab2d2d23f509
FboZhu <153364101+FboZhu@users.noreply.github.com>
2026-05-03 17:06:31 +0800

    更新 MSJR_DailyBonus.js

--- js/MSJR_DailyBonus.js
+++ js/MSJR_DailyBonus.js
@@ -28,7 +28,7 @@ const CONFIG = {
 const API_CONFIG = {
     BASE_URL: 'https://metaverse.rfc-friso.com',
     ENDPOINTS: {
-        USER_INFO: '/user/userInfo',
+        USER_INFO: '/exchange/userInfo',
         SIGN: '/task/signIn',
         INTERACTION: '/organicRanch/findInteraction'
     }
